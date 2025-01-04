6075fd72759689b67c52309c45de4365a9bd45c0
Vlad Korenev <korenevlad17@gmail.com>
2025-01-05 01:20:48 +0300

    Add Entity and VO

---
+++ src/YourPerfectRoute.Domain/Models/Entity.cs
@@ -0,0 +1,19 @@
+﻿using MediatR;
+
+namespace YourPerfectRoute.Domain.Models;
+public abstract class Entity
+{
+    private List<INotification> _domainEvents;
+    public List<INotification> DomainEvents => _domainEvents;
+
+    public void AddDomainEvent(INotification eventItem)
+    {
+        _domainEvents = _domainEvents ?? new List<INotification>();
+        _domainEvents.Add(eventItem);
+    }
+
+    public void RemoveDomainEvent(INotification eventItem)
+    {
+        _domainEvents?.Remove(eventItem);
+    }
+}
